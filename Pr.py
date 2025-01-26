3c0998a69881fd47dc714983eba6e50514f46290
Ayres <victorayres7@gmail.com>
2025-01-26 13:18:43 -0300

    Deleted unused files, updated Pr.py, and added Restaurant Menu.py

--- Pr.py
+++ Pr.py
@@ -1,5 +1,26 @@
-# Define the menu as a dictionary
-menu = {
-A, B, C, D = float(input())
+values = input().split(' ')
+A, B, C, D = map(float, values)
 
-Média = (A + B + C + D) / 4
+Media = (A + B + C + D) / 4
+
+print(f'Media: {Media:.1f}')
+if Media >= 7.0:
+    print("Aluno Aprovado")
+elif 5.0 <= Media < 7.0:
+    print('Aluno em Exame')
+else:
+    print('Aluno Reprovado')
+    exam_score = float(input("Nota do exame: "))
+    print(f'Nota do exame: {exam_score:.1f}')
+
+    # Recalculate the final average
+    final_media = (Media + exam_score) / 2
+
+    # Check if the student is approved or reproved after the exam
+    if final_media >= 5.0:
+        print("Aluno aprovado.")
+    else:
+        print("Aluno reprovado.")
+
+    # Print the final average
+    print(f'Media final: {final_media:.1f}')
