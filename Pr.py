4d70d71db07ceaee5eefd007388f4d472844c8b5
Ayres <victorayres7@gmail.com>
2025-01-27 12:59:14 -0300

    update

--- Pr.py
+++ Pr.py
@@ -1,26 +1,18 @@
 values = input().split(' ')
-A, B, C, D = map(float, values)
 
-Media = (A + B + C + D) / 4
+X , Y = map(float, values)
 
-print(f'Media: {Media:.1f}')
-if Media >= 7.0:
-    print("Aluno Aprovado")
-elif 5.0 <= Media < 7.0:
-    print('Aluno em Exame')
+if X == 0.0 and Y == 0.0:
+    print('Origem')
+elif(X==0):
+    print("Eixo Y")
+elif(Y==0):
+    print("Eixo X")
+elif X > 0.0 and Y > 0.0:
+    print('Q1')
+elif X < 0.0 and Y > 0.0:
+    print('Q2')
+elif X < 0.0 and Y < 0.0:
+    print('Q3')
 else:
-    print('Aluno Reprovado')
-    exam_score = float(input("Nota do exame: "))
-    print(f'Nota do exame: {exam_score:.1f}')
-
-    # Recalculate the final average
-    final_media = (Media + exam_score) / 2
-
-    # Check if the student is approved or reproved after the exam
-    if final_media >= 5.0:
-        print("Aluno aprovado.")
-    else:
-        print("Aluno reprovado.")
-
-    # Print the final average
-    print(f'Media final: {final_media:.1f}')
+    print('Q4')
