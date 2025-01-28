8e7150719d508e592530db06c66063f58432a07f
Ayres <victorayres7@gmail.com>
2025-01-28 16:16:27 -0300

    done

--- Pr.py
+++ Pr.py
@@ -1,18 +1,12 @@
 values = input().split(' ')
 
-X , Y = map(float, values)
+X, Y, Z = map(int, values)
 
-if X == 0.0 and Y == 0.0:
-    print('Origem')
-elif(X==0):
-    print("Eixo Y")
-elif(Y==0):
-    print("Eixo X")
-elif X > 0.0 and Y > 0.0:
-    print('Q1')
-elif X < 0.0 and Y > 0.0:
-    print('Q2')
-elif X < 0.0 and Y < 0.0:
-    print('Q3')
-else:
-    print('Q4')
+sorted_values = sorted([X, Y, Z])
+
+for value in sorted_values:
+    print(value)
+print("\n")
+
+for value in [X, Y, Z]:
+    print(value)
