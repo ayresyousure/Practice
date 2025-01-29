3862ad65bb290204229daa797b09b5fa5f3f3f2a
Ayres <victorayres7@gmail.com>
2025-01-29 16:43:28 -0300

    update

---
+++ challenge1.js
@@ -0,0 +1,33 @@
+/*const markWeight = 78
+const johnWeight = 92
+const markHeight = 169
+const johnHeight = 195
+
+const markBMI = markWeight / ((markHeight ** 2)/10000)
+const johnBMI = johnWeight / ((johnHeight ** 2)/10000)
+
+const markHigherBMI = markBMI > johnBMI
+
+console.log("Mark's BMI:", markBMI);
+console.log("John's BMI:", johnBMI);
+console.log("Does Mark have a higher BMI than John?", markHigherBMI);
+*/
+
+const matchDolphin1 = 96
+const matchDolphin2 = 108
+const matchDolphin3 = 89
+
+const matchKoala1 = 88
+const matchKoala2 = 91
+const matchKoala3 = 110
+
+const scoreDolphin = (matchDolphin1 + matchDolphin2 + matchDolphin3) / 3;
+const scoreKoalas = (matchKoala1 + matchKoala2 + matchKoala3) / 3;
+
+if (scoreDolphin > scoreKoalas){
+    console.log('Dolphins win the trophy')
+} else if (scoreKoalas > scoreDolphin){
+    console.log('Koalas win the trophy')
+} else {
+    console.log('Both win the trophy')
+}
